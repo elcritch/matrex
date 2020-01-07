c44cf38255c76099de76c74651da3a959addd00f
Jaremy Creechley <creechley@gmail.com>
2020-01-06 17:12:04 -0700

    ix rebase?
    removing extras deps

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,9 @@
 use Mix.Config
 
-case Mix.env() do
-  :dev ->
-    config :mix_test_watch,
-      clear: true,
-      extra_extensions: [
-        ".c"
-      ]
-
-  :test ->
-    config :excheck, :number_iterations, 200
+if Mix.env() == :dev do
+  config :mix_test_watch,
+    clear: true,
+    extra_extensions: [
+      ".c"
+    ]
 end
