049f5a8ffebbe23ea94714ce97f4b2a12c750cd4
Jaremy Creechley <creechley@gmail.com>
2020-01-06 17:09:45 -0700

    removing extras deps

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,9 @@
 use Mix.Config
 
-case Mix.env() do
-  :dev ->
-    config :mix_test_watch,
-      clear: true,
-      extra_extensions: [
-        ".c"
-      ]
-
+if Mix.env() == :dev do
+  config :mix_test_watch,
+    clear: true,
+    extra_extensions: [
+      ".c"
+    ]
 end
